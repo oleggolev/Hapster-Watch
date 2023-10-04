186190350e909344e82ff3fc248b49b5a6e9ddf7
Oleg Golev <ogolev@princeton.edu>
2023-10-04 18:08:05 -0400

    init

---
+++ HAPTIC-XCEL Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HAPTIC-XCEL Watch App
+//
+//  Created by Oleg Golev on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HAPTIC-XCEL Watch App/HAPTIC_XCELApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAPTIC_XCELApp.swift
+//  HAPTIC-XCEL Watch App
+//
+//  Created by Oleg Golev on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HAPTIC_XCEL_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
